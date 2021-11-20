30f0877fd4248ee9f201ec5670444a746261869b
コッコ先生 <jorjue@icloud.com>
2021-11-20 09:40:18 +0900

    slick追加

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-jQuery(function($) {
+jQuery(function ($) {
     $('#open').click(function () {
         $('#bgMenu').toggleClass('active');
+    });
+
+    $('.slider').slick({
+        centerMode: true,
+        centerPadding: '30px',
+        dots: true,
+        focusOnSelect: true,
+        dots: false,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
     });
 });
